5dba04f80eb99c777019a3be0de695e55e003b4b
dcuevas5 <dcuevas5@ivytech.edu>
2025-12-06 21:51:38 -0500

    added db

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 addEventListener("DOMContentLoaded", async function (){
     const response = await this.fetch("http://localhost:3000/api/songs");
     const songs = await response.json();
-let html = ""
+
+    let html = ""
     for (let song of songs){
         html += `<li>${song.title} - ${song.artist}</li>`
     }
